a7e712d45a0b0e3b1595af89a0ed40e9ec2501cf
Mason <mason_mchugh2@baylor.edu>
2024-03-25 15:36:50 -0500

    update classwork

--- list_comprehension_start_file.py
+++ list_comprehension_start_file.py
@@ -26,9 +26,87 @@ for i in original_list:
 
 #There are 3 parts to list comprehension:
 
-#*result*  = [*transform/expression*    *iteration*         *filter*     ]
+#*result*  = [*transform/expression*    *iteration*         *filter/condition*     ]
 
 #The filter part answers the question if the item should be transformed.
+
+
+old_list = [1,2,3,4,5]
+new_list = []
+
+for i in old_list:
+    j = i **2
+    new_list.append(j)
+
+print(new_list)
+
+new_list = [i**2 for i in old_list ]
+
+print(new_list)
+
+#1) creating a simple list of 10 numbers using range()
+x = [i for i in range(10)]
+print(x)
+
+#2) creating a list that evaluates an expression
+squares = [x**2 for x in range(10)]
+print(squares)
+
+#3) creating a list from another list
+list1 = [3,4,5]
+
+multiplied = [item*3 for item in list1]
+print(multiplied)
+
+#4) using list comprehension for string manipulation
+
+listofwords = ["The","Force","will","be","wuth","you.","Always."]
+
+items = [word[1].upper() for word in listofwords]
+print(items)
+
+#5) show lower case
+
+result = [x.lower() for x in ["A","B","C"]]
+print(result)
+
+#6) Creating a list based on a condition
+# create a list of the square of all even numbers from 1 to 10
+
+new_range = [i*i for i in range(1,11) if i%2 == 0]
+print(new_range)
+
+#7) Extracting numbers only from a string and putting it in a list
+string = "Hello 12345 world"
+numbers = [int(character) for character in string if character.isdigit()]
+print(numbers)
+letters = [x.upper() for x in string.split(' ') if x.isalpha()]
+print(letters)
+
+
+#8) get a specific line from a text file
+
+infile = open('test.txt','r')
+
+result = [i.rstrip('\n') for i in infile if 'line3' in i]
+print(result)
+
+#9) Using functions in list comprehension
+
+def double(x):
+    return x * 2
+
+
+result = [double(x) for x in range(1,11) if x % 2 ==1]
+print(result)
+
+#10) you can add more arguments (using multiple iterators and lists):
+
+a = [(x,y) for x in [10,50] for y in [20,40]]
+print(a)
+
+a = [x+y for x in [10,50] for y in [20,40] if x+y>70]
+print(a)
 
 
 
@@ -70,7 +148,7 @@ dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400,
 
 ## Extract the numbers from the following phrase ##
 
-phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each " +
+phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each " \
 "event, with about 3 or 4 that were classifled as serious per event.'
 
 
